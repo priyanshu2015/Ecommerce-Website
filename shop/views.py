a3e21fc82fad68dc5ab5693274dad198c88b850a
Priyanshu Gupta <priyanshu.gupta2008@gmail.com>
2020-10-11 09:15:17 +0530

    check db xss

--- shop/views.py
+++ shop/views.py
@@ -45,6 +45,7 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)   # saving in dataset just like by using python as in shell
         contact.save()
+        return render(request, 'shop/contact.html', {'query': contact.name})
         if 'name' in request.POST:
             q = request.POST['name']
             return render(request, 'shop/contact.html', {'query': q})
