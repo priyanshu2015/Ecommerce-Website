460820c8dca20cbfa38c75ed3e04818cc2e4f509
Priyanshu Gupta <priyanshu.gupta2008@gmail.com>
2020-10-26 22:48:49 +0530

    adding samesite lax sessioid

--- mac/settings.py
+++ mac/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'shop.middleware.SameSiteMiddleware',# position it at the top
+    # 'shop.middleware.SameSiteMiddleware',# position it at the top
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
